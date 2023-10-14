500598169b99444ea87fd2c0e28ffe5faa333c59
addmitriev66 <83495344+addmitriev66@users.noreply.github.com>
2023-10-14 18:21:24 +0300

    Add files via upload

---
+++ homework04/optimization.py
@@ -0,0 +1,161 @@
+import numpy as np
+from numpy.linalg import LinAlgError
+import scipy
+from datetime import datetime
+from collections import defaultdict
+from scipy.optimize import minimize_scalar, scalar_search_wolfe2
+import time
+from scipy.linalg import cho_factor, cho_solve
+
+
+
+class LineSearchTool(object):
+    def __init__(self, method='Wolfe', **kwargs):
+        self._method = method
+        if self._method == 'Wolfe':
+            self.c1 = kwargs.get('c1', 1e-4)
+            self.c2 = kwargs.get('c2', 0.9)
+            self.alpha_0 = kwargs.get('alpha_0', 1.0)
+        elif self._method == 'Armijo':
+            self.c1 = kwargs.get('c1', 1e-4)
+            self.alpha_0 = kwargs.get('alpha_0', 1.0)
+        elif self._method == 'Constant':
+            self.c = kwargs.get('c', 1.0)
+        else:
+            raise ValueError('Unknown method {}'.format(method))
+
+    @classmethod
+    def from_dict(cls, options):
+        if type(options) != dict:
+            raise TypeError('LineSearchTool initializer must be of type dict')
+        return cls(**options)
+
+    def to_dict(self):
+        return self.__dict__
+
+
+    def line_search(self, oracle, x_k, d_k, previous_alpha=None):
+        if self.method == 'Wolfe':
+            # Попытайтесь использовать библиотечную функцию scalar_search_wolfe2
+            # Если она не сходится, запустите процедуру дробления шага
+            try:
+                alpha = scalar_search_wolfe2(lambda alpha: oracle.func_directional(x_k, d_k, alpha),
+                                              lambda alpha: oracle.grad_directional(x_k, d_k, alpha),
+                                              c1=self.kwargs.get('c1', 1e-4), c2=self.kwargs.get('c2', 0.9)).alpha
+            except (ValueError, RuntimeError):
+                alpha = self.backtracking_line_search(oracle, x_k, d_k, previous_alpha)
+        else:
+            # Если метод не 'Wolfe', используйте процедуру дробления шага
+            alpha = self.backtracking_line_search(oracle, x_k, d_k, previous_alpha)
+        return alpha
+
+    def backtracking_line_search(self, oracle, x_k, d_k, previous_alpha=None):
+        # Начальное значение шага
+        alpha = 1.0 if previous_alpha is None else previous_alpha
+
+        # Параметры для процедуры дробления шага
+        rho = self.kwargs.get('rho', 0.5)  # Коэффициент уменьшения шага (обычно 0.5)
+        c = self.kwargs.get('c', 1e-4)  # Параметр условия Армихо
+
+        while True:
+            # Вычисляем значение функции и градиента для текущего шага
+            f_val = oracle.func(x_k + alpha * d_k)
+            grad_val = oracle.grad(x_k + alpha * d_k)
+
+            # Проверяем условие Армихо
+            if f_val <= oracle.func(x_k) + c * alpha * np.dot(grad_val, d_k):
+                return alpha  # Успешно найден подходящий шаг
+
+            # Уменьшаем шаг и продолжаем итерации
+            alpha *= rho
+
+def get_line_search_tool(line_search_options=None):
+    if line_search_options:
+        if type(line_search_options) is LineSearchTool:
+            return line_search_options
+        else:
+            return LineSearchTool.from_dict(line_search_options)
+    else:
+        return LineSearchTool()
+
+
+
+def gradient_descent(oracle, x_0, tolerance=1e-5, max_iter=10000,
+                     line_search_options=None, trace=False, display=False):
+
+    history = defaultdict(list) if trace else None
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = np.copy(x_0)
+
+    for iter_num in range(max_iter):
+        # Вычисление градиента в текущей точке
+        gradient_xk = oracle.grad(x_k)
+
+        # Проверка критерия останова (норма градиента)
+        grad_norm = np.linalg.norm(gradient_xk)
+        if grad_norm < tolerance:
+            return x_k, 'success', history
+
+        # Вычисление направления поиска (отрицательный градиент)
+        search_direction = -gradient_xk
+
+        # Выполнение линейного поиска для определения размера шага (alpha)
+        alpha = line_search_tool.line_search(oracle, x_k, search_direction)
+
+        # Обновление текущей точки
+        x_k = x_k + alpha * search_direction
+
+        # Обновление истории для отслеживания
+        if trace:
+            history['time'].append(datetime.now())
+            history['func'].append(oracle.func(x_k))
+            history['grad_norm'].append(grad_norm)
+            if x_k.size <= 2:
+                history['x'].append(np.copy(x_k))
+
+    # Если достигнуто максимальное количество итераций
+    return x_k, 'iterations_exceeded', history
+
+
+
+def newton(oracle, x_0, tolerance=1e-5, max_iter=100,
+           line_search_options=None, trace=False, display=False):
+    history = defaultdict(list) if trace else None
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = np.copy(x_0)
+
+    for iteration in range(max_iter):
+        # Вычисление градиента и гессиана
+        grad_k = oracle.grad(x_k)
+        hess_k = oracle.hess(x_k)
+        
+        # Вычисление направление Ньютона
+        newton_direction = -np.linalg.solve(hess_k, grad_k)
+        
+        # Поиск строки
+        alpha_k, _ = line_search_tool.line_search(oracle, x_k, newton_direction)
+        
+        # Обновление итерации
+        x_k += alpha_k * newton_direction
+        
+        # Вычисление значения функции и норму градиента
+        func_value = oracle.func(x_k)
+        grad_norm = np.linalg.norm(grad_k)
+        
+        # Сохранение значений 
+        if trace:
+            history['time'].append(time.time())
+            history['func'].append(func_value)
+            history['grad_norm'].append(grad_norm)
+            if x_k.size <= 2:
+                history['x'].append(np.copy(x_k))
+        
+        # Проверка критерий сходимости
+        if grad_norm < tolerance:
+            return x_k, 'success', history
+
+    return x_k, 'iterations_exceeded', history
+
+
+
+
---
+++ homework04/oracles.py
@@ -0,0 +1,134 @@
+import numpy as np
+import scipy
+from scipy.special import expit, issparse
+
+
+class BaseSmoothOracle(object):
+    """
+    Базовый класс для реализации оракулов.
+    """
+    def func(self, x):
+        """
+        Вычисляет значение функции в точке x.
+        """
+        raise NotImplementedError('Func oracle is not implemented.')
+
+    def grad(self, x):
+        """
+        Вычисляет градиент в точке x.
+        """
+        raise NotImplementedError('Grad oracle is not implemented.')
+    
+    def hess(self, x):
+        """
+        Вычисляет гессиан в точке x.
+        """
+        raise NotImplementedError('Hessian oracle is not implemented.')
+    
+    def func_directional(self, x, d, alpha):
+        """
+        Вычисляет phi(alpha) = f(x + alpha*d).
+        """
+        return np.squeeze(self.func(x + alpha * d))
+
+    def grad_directional(self, x, d, alpha):
+        """
+        Вычисляет phi'(alpha) = (f(x + alpha*d))'_{alpha}
+        """
+        return np.squeeze(self.grad(x + alpha * d).dot(d))
+
+
+class QuadraticOracle(BaseSmoothOracle):
+    def __init__(self, A, b):
+        if not scipy.sparse.isspmatrix_dia(A) and not np.allclose(A, A.T):
+            raise ValueError('A should be a symmetric matrix.')
+        self.A = A
+        self.b = b
+
+    def func(self, x):
+        return 0.5 * np.dot(self.A.dot(x), x) - self.b.dot(x)
+
+    def grad(self, x):
+        return self.A.dot(x) - self.b
+
+    def hess(self, x):
+        return self.A 
+
+
+class LogRegL2Oracle(BaseSmoothOracle):
+    def __init__(self, matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef):
+        self.matvec_Ax = matvec_Ax
+        self.matvec_ATx = matvec_ATx
+        self.matmat_ATsA = matmat_ATsA
+        self.b = b
+        self.regcoef = regcoef
+
+    def func(self, x):
+        z = self.matvec_Ax(x)
+        return np.mean(np.logaddexp(0, -self.b * z)) + 0.5 * self.regcoef * np.linalg.norm(x)**2
+
+    def grad(self, x):
+        z = self.matvec_Ax(x)
+        sigmoid_term = expit(-self.b * z)
+        gradient = -self.matvec_ATx(self.b * sigmoid_term) / len(self.b) + self.regcoef * x
+        return gradient
+
+    def hess(self, x):
+        z = self.matvec_Ax(x)
+        sigmoid_term = expit(-self.b * z)
+        diag_sigmoid = sigmoid_term * (1 - sigmoid_term)
+        hessian = self.matmat_ATsA(diag_sigmoid) / len(self.b) + self.regcoef * np.identity(len(x))
+        return hessian
+
+
+class LogRegL2OptimizedOracle(LogRegL2Oracle):
+    def __init__(self, matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef):
+        super().__init__(matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef)
+
+    def func_directional(self, x, d, alpha):
+        Ax = self.matvec_Ax(x)
+        Ad = self.matvec_Ax(d)
+        return np.mean(np.logaddexp(0, -self.b * (Ax + alpha * Ad))) + 0.5 * self.regcoef * np.linalg.norm(x + alpha * d) ** 2
+
+    def grad_directional(self, x, d, alpha):
+        Ax = self.matvec_Ax(x)
+        Ad = self.matvec_Ax(d)
+        exp_term = -self.b * (Ax + alpha * Ad)
+        coef = -self.b * np.exp(expit(exp_term))
+        gradient = self.matvec_ATx(coef) + self.regcoef * (x + alpha * d)
+        return gradient
+
+
+def create_log_reg_oracle(A, b, regcoef, oracle_type='usual'):
+    if issparse(A):
+        matvec_Ax = lambda x: A.dot(x)
+        matvec_ATx = lambda x: A.T.dot(x)
+        matmat_ATsA = lambda s: (A.T * s).dot(A)
+    else:
+        matvec_Ax = lambda x: A @ x
+        matvec_ATx = lambda x: A.T @ x
+        matmat_ATsA = lambda s: A.T @ (s[:, None] * A)
+    
+    if oracle_type == 'usual':
+        oracle = LogRegL2Oracle(matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef)
+    elif oracle_type == 'optimized':
+        oracle = LogRegL2OptimizedOracle(matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef)
+    else:
+        raise ValueError('Unknown oracle_type: {}'.format(oracle_type))
+    
+    return oracle
+
+
+def grad_finite_diff(func, x, eps=1e-8):
+    n = len(x)
+    gradient = np.zeros(n)
+    for i in range(n):
+        x_plus_eps = x.copy()
+        x_plus_eps[i] += eps
+        gradient[i] = (func(x_plus_eps) - func(x)) / eps
+    return gradient
+
+
+def hess_finite_diff(func, x, eps=1e-5):
+    n = len(x)
+    hessian = np.zeros((n, n))
